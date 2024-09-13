dc56bf64138a05da373b752b184d7bfd1f046ebc
Pudit Chokmeesuk <pudit.chok@gmail.com>
2024-09-13 15:50:47 +0700

    Update project detail

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,23 +7,34 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      {props.iframeSrc ? (
+        <div style={{ width: "100%", height: "230px", overflow: "hidden" }}>
+          <iframe
+            src={props.iframeSrc}
+            title={props.title}
+            style={{
+              width: "100%",
+              height: "100%",
+              border: "none",
+            }}
+          />
+        </div>
+      ) : (
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      )}
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
         {props.ghLink && (
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
+          </Button>
         )}
         {"\n"}
         {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
         {!props.isBlog && props.demoLink && (
           <Button
             variant="primary"
@@ -32,11 +43,12 @@ function ProjectCards(props) {
             style={{ marginLeft: "10px" }}
           >
             <CgWebsite /> &nbsp;
-            {"Play"}
+            {"Visit"}
           </Button>
         )}
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import mysteryagents from "../../Assets/Projects/mysteryagents.png";
+import btcclock from "../../Assets/Projects/btc-clock.png";
+import regolich from "../../Assets/Projects/regolich.png";
 
 function Projects() {
   return (
@@ -19,11 +21,27 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={mysteryagents}
-              isBlog={false}
               title="Mystery Agents"
               description="Board game on mobile"
-              // ghLink="https://github.com/oampudit"
               demoLink="https://mystery-agents.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={btcclock}
+              title="BTC Clock"
+              description="BTC Clock is a web app that displays real-time Bitcoin block height, price, satoshis per unit of currency, local time, and halving estimate."
+              demoLink="https://btc-clock.firebaseapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={regolich}
+              title="RegoLich"
+              description='RegoLich combines MOBA and Battle Royale genres with Bitcoin integration via Lightning Network and Nostr for decentralized transactions.'
+              demoLink="https://regolich-project.firebaseapp.com/"
             />
           </Col>
         </Row>
